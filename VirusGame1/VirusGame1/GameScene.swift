0e1df35d4ee59351147aa552cd69a147ceebf78a
Kittyjelly19 <49998740+Kittyjelly19@users.noreply.github.com>
2020-12-13 09:02:46 -0800

    blank project start

---
+++ VirusGame1/VirusGame1/GameScene.swift
@@ -0,0 +1,22 @@
+//
+//  GameScene.swift
+//  VirusGame1
+//
+//  Created by SYMES, BETHAN (Student) on 13/12/2020.
+//  Copyright © 2020 SYMES, BETHAN (Student). All rights reserved.
+//
+
+import SpriteKit
+
+
+class GameScene: SKScene
+{
+    
+    override func didMove(to view: SKView)
+    {
+        
+        
+    }
+        
+        
+}
---
+++ VirusGame1/VirusGame1/GameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameViewController.swift
+//  VirusGame1
+//
+//  Created by SYMES, BETHAN (Student) on 13/12/2020.
+//  Copyright © 2020 SYMES, BETHAN (Student). All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            let scene = GameScene(size: view.bounds.size)
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    
+}
