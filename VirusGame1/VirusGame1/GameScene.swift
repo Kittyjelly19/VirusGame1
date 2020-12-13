995383fb558349bcea1ca397515095dcae81817d
Kittyjelly19 <49998740+Kittyjelly19@users.noreply.github.com>
2020-12-13 09:23:38 -0800

    added player representation to bottom of screen.
    Size and location of player subject to change, tested on simulator.

--- VirusGame1/VirusGame1/GameScene.swift
+++ VirusGame1/VirusGame1/GameScene.swift
@@ -11,12 +11,24 @@ import SpriteKit
 
 class GameScene: SKScene
 {
+    var player:SKSpriteNode!
     
     override func didMove(to view: SKView)
     {
-        
+        initScene()
+       
         
     }
+       func initScene()
+       {
         
+        backgroundColor = UIColor(red: 44/255, green: 62/255, blue: 80/255, alpha: 1.0)
+        
+        player = SKSpriteNode(imageNamed: "ball")
+        player.size = CGSize(width: frame.size.width/3, height: frame.size.width/3)
+               player.position = CGPoint(x: self.frame.size.width / 2, y: player.size.height / 2 + 20)
+               self.addChild(player)
+        
+    }
         
 }
