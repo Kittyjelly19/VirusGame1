28d4107ef90bb4b255d99cb261acecd4892284e5
Kittyjelly19 <49998740+Kittyjelly19@users.noreply.github.com>
2020-12-13 14:07:35 -0800

    additions and cleaning up.
    -Added shooting of hand sanitizer in direcion of virus flow.
    -Cleaned up some code.
    -Added bitmasks for virus and sanitizer nodes for collision.

--- VirusGame1/VirusGame1/GameScene.swift
+++ VirusGame1/VirusGame1/GameScene.swift
@@ -13,13 +13,12 @@ class GameScene: SKScene, SKPhysicsContactDelegate
 {
     var player:SKSpriteNode!
     var virus:SKSpriteNode!
+    var sanitizerBlob:SKSpriteNode!
     var scoreText:SKLabelNode!
     var gameTimerHandle:Timer!
     var virusVariants = ["ball", "ColorCircle"]
     
-    //Properties that allow for each category to have a unique ID to detect collisions.
-    let virusCat:UInt32 = 0x1 << 1
-    let sanitizerBullet:UInt32
+    
     
     var score: Int = 0
     {
@@ -38,16 +37,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate
        
         
     }
+    //Properties that allow for each category to have a unique ID to detect collisions.
+    let virusCat:UInt32 = 0x1 << 1
+    let sanitizerBlobCat:UInt32 = 0x1 << 0
+    
     
     //Initial scene render function.
     func InitScene()
        {
         
         //Set a border around the screen to prevent virus from leaving screen.
-        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
-        border.friction = 0
-        border.restitution = 1
-        self.physicsBody = border
+        //let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+       // border.friction = 0
+       // border.restitution = 1
+       // self.physicsBody = border
         
         //Set background colour of game.
         backgroundColor = UIColor(red: 45/255, green: 40/255, blue: 80/255, alpha: 1.0)
@@ -72,7 +75,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate
        
         virus.physicsBody = SKPhysicsBody(circleOfRadius: virus.size.width / 2)
         virus.physicsBody?.isDynamic = true
-         self.addChild(virus)
+        
+        virus.physicsBody?.categoryBitMask = virusCat
+        virus.physicsBody?.contactTestBitMask = sanitizerBlobCat
+        virus.physicsBody?.collisionBitMask = 0
+        
+        self.addChild(virus)
+        
+        let movementDur:TimeInterval = 6.0
+        var movementArray = [SKAction]()
+        
+        movementArray.append(SKAction.move(to: CGPoint(x: position, y: -virus.size.height), duration: movementDur))
+        movementArray.append(SKAction.removeFromParent())
+        
+        virus.run(SKAction.sequence(movementArray))
+        
     }
     
     //Spawn player function
@@ -95,5 +112,36 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         scoreText.fontColor = UIColor.magenta
         score = 0
         self.addChild(scoreText)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        ShootSanitizer()
+    }
+    func ShootSanitizer()
+    {
+        sanitizerBlob = SKSpriteNode(imageNamed: "ball")
+        sanitizerBlob.size = CGSize(width: 20, height:20)
+        sanitizerBlob.position = player.position
+        sanitizerBlob.position.y  += 5
+        
+        sanitizerBlob.physicsBody = SKPhysicsBody(circleOfRadius: sanitizerBlob.size.width / 2)
+        sanitizerBlob.physicsBody?.isDynamic = true
+        
+        sanitizerBlob.physicsBody?.categoryBitMask = sanitizerBlobCat
+        sanitizerBlob.physicsBody?.contactTestBitMask = virusCat
+        sanitizerBlob.physicsBody?.collisionBitMask = 0
+        
+        sanitizerBlob.physicsBody?.usesPreciseCollisionDetection = true
+        
+        self.addChild(sanitizerBlob)
+        
+        let movementDur:TimeInterval = 0.3
+        var movementArray = [SKAction]()
+        
+        movementArray.append(SKAction.move(to: CGPoint(x: player.position.x, y: self.frame.size.height + 10), duration: movementDur))
+        movementArray.append(SKAction.removeFromParent())
+        
+        sanitizerBlob.run(SKAction.sequence(movementArray))
     }
 }
