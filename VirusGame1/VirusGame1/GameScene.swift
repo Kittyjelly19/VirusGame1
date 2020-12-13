0c9ff9a98c27608ba6a5c4ad3b3d60d2b8378a2e
Kittyjelly19 <49998740+Kittyjelly19@users.noreply.github.com>
2020-12-13 10:28:54 -0800

    player set to bottom of screen, virus set to top

--- VirusGame1/VirusGame1/GameScene.swift
+++ VirusGame1/VirusGame1/GameScene.swift
@@ -12,23 +12,45 @@ import SpriteKit
 class GameScene: SKScene
 {
     var player:SKSpriteNode!
+    var virus:SKSpriteNode!
     
     override func didMove(to view: SKView)
     {
-        initScene()
+        InitScene()
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
        
         
     }
-       func initScene()
+    //Initial scene render function.
+    func InitScene()
        {
         
-        backgroundColor = UIColor(red: 44/255, green: 62/255, blue: 80/255, alpha: 1.0)
+        backgroundColor = UIColor(red: 44/255, green: 40/255, blue: 80/255, alpha: 1.0)
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        border.friction = 0
+        border.restitution = 1
         
+        self.physicsBody = border
+        
+        //Adding player to scene
         player = SKSpriteNode(imageNamed: "ball")
         player.size = CGSize(width: frame.size.width/3, height: frame.size.width/3)
-               player.position = CGPoint(x: self.frame.size.width / 2, y: player.size.height / 2 + 20)
-               self.addChild(player)
+        player.position = CGPoint(x: frame.midX, y: frame.minY + player.size.height)
+        self.addChild(player)
+        SpawnVirus()
         
     }
-        
+    
+    //Spawn virus function.
+    func SpawnVirus()
+    {
+        virus = SKSpriteNode(imageNamed: "ball")
+        virus.size = CGSize(width: frame.size.width/3, height: frame.size.width/3)
+        virus.position = CGPoint(x: frame.midX, y: frame.maxY - virus.size.height)
+       
+        let ballcol = SKPhysicsBody(circleOfRadius: virus.size.width)
+        self.physicsBody = ballcol
+        virus.physicsBody?.applyImpulse(CGVector(dx:0, dy: -20))
+         self.addChild(virus)
+    }
 }
