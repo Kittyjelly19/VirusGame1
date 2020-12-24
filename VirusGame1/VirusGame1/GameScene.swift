d585c14e7ec2304cef13de6ff487e55b06b81dea
Kittyjelly19 <49998740+Kittyjelly19@users.noreply.github.com>
2020-12-24 11:14:02 -0800

    additions and fixes
    -Added working score system
    
    -Added working health system
    
    -fixed player collisions
    
    -TO DO:
    *add menus for the game loop.
    * fix health text placement for app use on ipad.

--- VirusGame1/VirusGame1/GameScene.swift
+++ VirusGame1/VirusGame1/GameScene.swift
@@ -9,14 +9,16 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene, SKPhysicsContactDelegate
-{
+class GameScene: SKScene, SKPhysicsContactDelegate{
+
     var player:SKSpriteNode!
     var virus:SKSpriteNode!
     var sanitizerBlob:SKSpriteNode!
     var scoreText:SKLabelNode!
+    var healthText:SKLabelNode!
     var gameTimerHandle:Timer!
     var virusVariants = ["virus", "virus2"]
+    
     
     
     
@@ -29,18 +31,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         }
     }
     
+    var health: Int = 100
+    {
+        didSet
+        {
+            healthText.text = "Health: \(health)"
+        }
+    }
     override func didMove(to view: SKView)
     {
-         let screenBounds = SKPhysicsBody(edgeLoopFrom: self.frame)
-        self.physicsBody = screenBounds
-        self.physicsBody!.affectedByGravity = false
-        self.physicsBody!.usesPreciseCollisionDetection = true
-        self.physicsBody!.mass = 0
-        self.physicsBody!.restitution = 1
-        
-        //self.physicsBody!.categoryBitMask = screenBoundsCat
-        //self.physicsBody!.contactTestBitMask = virusCat
-         self.physicsBody?.collisionBitMask = 0
         InitScene()
         self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
         
@@ -50,7 +49,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate
     //Properties that allow for each category to have a unique ID to detect collisions.
     let virusCat:UInt32 = 0x1 << 1
     let sanitizerBlobCat:UInt32 = 0x1 << 0
-    //let screenBoundsCat:UInt32 = 0x1 << 2
+    let playerCat:UInt32 = 0x1 << 2
    
     
     //Initial scene render function.
@@ -58,19 +57,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate
        {
         
         
-        //Set a border around the screen to prevent virus from leaving screen.
-        //let border = SKPhysicsBody(edgeLoopFrom: self.frame)
-       //border.friction = 0
-       //border.restitution = 1
-       //self.physicsBody = border
-        
         //Set background colour of game.
         backgroundColor = UIColor(red: 45/255, green: 40/255, blue: 80/255, alpha: 1.0)
         
         //Spawn scene components
         SpawnPlayer()
         DisplayScore()
-        
+        DisplayHealth()
         gameTimerHandle = Timer.scheduledTimer(timeInterval: 1.25, target: self, selector: #selector(SpawnVirus), userInfo: nil, repeats: true)
     }
     
@@ -78,8 +71,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate
     @objc func SpawnVirus()
     {
         virusVariants = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: virusVariants) as! [String]
-        let virus = SKSpriteNode(imageNamed: virusVariants[1])
-        let randVirusPos = GKRandomDistribution(lowestValue: 100, highestValue: 395)
+        let virus = SKSpriteNode(imageNamed: virusVariants[0])
+        let randVirusPos = GKRandomDistribution(lowestValue: 20, highestValue: 400)
         let position = CGFloat(randVirusPos.nextInt())
         virus.position = CGPoint(x: position, y: self.frame.size.height + virus.size.height)
         virus.size = CGSize(width: 100, height: 100)
@@ -92,7 +85,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         
         virus.physicsBody?.categoryBitMask = virusCat
         virus.physicsBody?.contactTestBitMask = sanitizerBlobCat
-        //virus.physicsBody?.contactTestBitMask = screenBoundsCat
+       
         virus.physicsBody?.collisionBitMask = 0
        
         
@@ -101,6 +94,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         let movementDur:TimeInterval = 8.0
         var movementArray = [SKAction]()
         
+        //if(virus.position.x >= frame.maxX) //|| virus.position.x >= frame.minX)
+       // {
+            //movementArray.append(SKAction.applyImpulse(CGVector(dx:50, dy:50), duration: 2))
+       // }
         //movementArray.append(SKAction.applyImpulse(CGVector(dx: 50, dy: 50), duration: 3))
         movementArray.append(SKAction.move(to: CGPoint(x: position, y: -virus.size.height), duration: movementDur))
         movementArray.append(SKAction.removeFromParent())
@@ -114,8 +111,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate
     {
         //Adding player to scene
         player = SKSpriteNode(imageNamed: "bottle")
-        player.size = CGSize(width: frame.size.width/2.5, height: frame.size.width/2.5)
         player.position = CGPoint(x: frame.midX, y: frame.minY + player.size.height)
+        player.size = CGSize(width:100, height:150)
+        
+        player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.height / 2)
+        player.physicsBody?.isDynamic = true
+        player.physicsBody?.categoryBitMask = playerCat
+        player.physicsBody?.contactTestBitMask = virusCat
+         player.physicsBody?.usesPreciseCollisionDetection = true
+        player.physicsBody?.collisionBitMask = 0
+        
         self.addChild(player)
         self.physicsWorld.contactDelegate = self
     }
@@ -129,6 +134,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         scoreText.fontColor = UIColor.magenta
         score = 0
         self.addChild(scoreText)
+    }
+    
+    func DisplayHealth()
+    {
+        healthText = SKLabelNode(text: "Score: 0")
+        healthText.position = CGPoint(x:300, y:self.frame.size.height - 100)
+        healthText.fontSize = 40
+        healthText.fontColor = UIColor.green
+        health = 100
+        self.addChild(healthText)
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
@@ -156,6 +171,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         let movementDur:TimeInterval = 0.3
         var movementArray = [SKAction]()
         
+    
         movementArray.append(SKAction.move(to: CGPoint(x: player.position.x, y: self.frame.size.height + 10), duration: movementDur))
         movementArray.append(SKAction.removeFromParent())
         
@@ -178,18 +194,28 @@ class GameScene: SKScene, SKPhysicsContactDelegate
             secondBody = contact.bodyA
         }
         
-        //UNCOMMENT THIS TO TURN ON COLLISION (currently broken)
-       // if (firstBody.categoryBitMask & sanitizerBlobCat) != 0 && (secondBody.categoryBitMask & virusCat) != 0
-       // {
-           // blob_virusCollision(sanitizerBlob: firstBody.node as! SKSpriteNode, virus: secondBody.node as! SKSpriteNode)
-       // }
+        //Checks collision between virus and sanitizer blob.
+       if (firstBody.categoryBitMask & sanitizerBlobCat) != 0 && (secondBody.categoryBitMask & virusCat) != 0
+       {
+            blob_virusCollision(sanitizerBlob: firstBody.node as! SKSpriteNode, virus: secondBody.node as! SKSpriteNode)
+        }
         
+        if(firstBody.categoryBitMask & virusCat) != 0 && (secondBody.categoryBitMask & playerCat) != 0
+        {
+            virus_playerCollision(virus: firstBody.node as! SKSpriteNode , player: secondBody.node as! SKSpriteNode)
+        }
     }
     
-    
+    func virus_playerCollision (virus:SKSpriteNode, player:SKSpriteNode)
+    {
+        virus.removeFromParent()
+        health -= 5
+    }
     func blob_virusCollision (sanitizerBlob:SKSpriteNode, virus:SKSpriteNode)
     {
     sanitizerBlob.removeFromParent()
     virus.removeFromParent()
+        score += 10
     }
 }
+
